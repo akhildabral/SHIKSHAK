301d5d1ba72de3e525fc61bbdb5177c505b336af
Lokesh <lokeshvishnoi1@gmail.com>
2017-01-24 02:09:28 +0530

    splash+1-24
    splash screen

---
+++ App/Shikshak/app/src/main/java/com/tutor/shikshak/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.tutor.shikshak;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();  //close this activity
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
